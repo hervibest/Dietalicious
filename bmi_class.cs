ad99e6f5e60aabd59569c2450b844144ef17592d
Auletta Khansa Pradiviasari <82350726+Auletta-Khansa@users.noreply.github.com>
2022-09-20 05:41:55 +0700

    Add files via upload

---
+++ bmi_class.cs
@@ -0,0 +1,17 @@
+class BMI //Class yang berisikan perhitungan BMI
+{
+    private float weight;
+    private float height;
+
+    public BMI(float weight, float height)
+    {
+        weight = BB;
+        height = TB;
+    }
+    
+    public calcBMI(float weight, float height)
+    {
+        BMI = (weight*703) / (height*height);
+        return BMI;
+    }
+}
