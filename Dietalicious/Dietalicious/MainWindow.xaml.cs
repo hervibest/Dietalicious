cfa37c8de470c3b72c5330a590e8c994fd199a4d
HerviBest <hervipro@gmail.com>
2022-11-07 16:07:38 +0700

    Add login and sign up method belum fix

--- Dietalicious/Dietalicious/MainWindow.xaml.cs
+++ Dietalicious/Dietalicious/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
+using System.Data;
+
 
 namespace Dietalicious
 {
@@ -20,16 +23,53 @@ namespace Dietalicious
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NpgsqlConnection conn;
+        string connstring = "Host=mydatabase-instance.csbnsdtoskt5.ap-northeast-1.rds.amazonaws.com;Username=postgres;Password=informatika;Database=Dietalicious_database";
+        public static NpgsqlCommand cmd;
+        private string sql = null; 
         public MainWindow()
         {
             InitializeComponent();
+            conn = new NpgsqlConnection(connstring);
+            conn.Open();
+            conn.Close();
+
+
         }
 
         private void Login(object sender, RoutedEventArgs e)
         {
-            Hide();
-            Home home = new Home();
-            home.Show();
+            try
+            {
+             
+                    conn.Open();
+                    sql = @"select * from st_logins(:_user_name  ,:_password)";
+                    cmd = new NpgsqlCommand(sql, conn);
+
+                    cmd.Parameters.AddWithValue("_user_name", tbUsername.Text);
+                    cmd.Parameters.AddWithValue("_password", tbPassword.Password);
+                    if ((int)cmd.ExecuteScalar() == 1)
+                    {
+                        MessageBox.Show("Data Users berhasil ditemukan", "Well Done!");
+                        conn.Close();
+                    Hide();
+                    Home home = new Home();
+                    home.Show();
+                }
+                else
+                {
+                    MessageBox.Show("User belum dibuat atau Password Salah");
+                }
+                    conn.Close();
+                
+                
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message, "Login FAIL!!");
+            }
+            
         }
         private void Register(object sender, RoutedEventArgs e)
         {
