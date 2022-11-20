936b5b7bc3fe64d460f7f4333f7c85817f96c811
Hervi Nur Rahmandien <71449881+hervibest@users.noreply.github.com>
2022-11-20 14:43:39 +0700

    Enable resource on Logo

--- Dietalicious/Dietalicious/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Dietalicious/Dietalicious/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Dietalicious {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Dietalicious {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public static void Main() {
             Dietalicious.App app = new Dietalicious.App();
             app.InitializeComponent();
